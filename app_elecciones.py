996c347eaaeff41b9b2bd119b9d665c1bd3b375d
diespeso <edgar.froot@gmail.com>
2020-12-05 13:22:22 -0800

    implementado sistema de moda de votos

--- app_elecciones.py
+++ app_elecciones.py
@@ -8,6 +8,7 @@ en orden de importancia
 from generador import Generador
 from distribuidor_tabla import DistribuidorDeTabla
 import copy
+import statistics
 
 N_VOTOS = 3
 
@@ -64,6 +65,7 @@ class Votante:
 		if self.contador_votos == 0:
 			self.distro = distribucion # solo aceptar la distro si es nuevo votante
 		if self.contador_votos < self.max_votos:
+			#print(self.contador_votos, self.max_votos)
 			self.votos.append(
 				self.distro.obtener_evento(pseudo)
 			)
@@ -75,6 +77,12 @@ class Votante:
 			
 			return self.votos[c]
 		self.reiniciar() # ya se votó N veces
+		"""Si solo reinicio, entonces se pierde siempre el número
+		que triggereó el reiniciado, entonces también voto
+		luego de reinicar para no desperdiciar números,
+		es una función recursiva
+		"""
+		self.votar(pseudo, distribucion) 
 
 	def reiniciar(self):
 		self.votos = []
@@ -117,6 +125,148 @@ def redistribuir_tabla(ev, distro):
 	else:
 		raise Exception("no se pudo redistribuir el evento dado") # ignorar y enviar denuevo
 
+class Votacion:
+	"""Toma una cantidad de votantes, y simula las votaciones
+	para 5 sectores:
+	sector 1: 16%
+	sector 2: 24%
+	sector 3: 20%
+	sector 4: 5%
+	sector 5: 15%
+
+	Considerar que los votantes deben de ser el 5% de 200,000
+	"""
+
+	def __init__(self, c_votantes, distro, numeros):
+		"""c_votantes = cantidad de votantes
+		distro = tabla de distribución de probabilidad
+		numeros = arreglo de números pseudoaleatorios
+		"""
+		self.c_votantes = c_votantes
+		self.votante = Votante()
+		self.distro = distro
+		self.numeros = numeros
+
+		self.votos_sector = {}
+		self.votos_sector[1] = int(round(c_votantes * 0.16, 0)) #sector 1
+		self.votos_sector[2] = int(round(c_votantes * 0.24, 0)) #s2
+		self.votos_sector[3] = int(round(c_votantes * 0.20, 0))
+		self.votos_sector[4] = int(round(c_votantes * 0.05, 0))
+		self.votos_sector[5] = int(round(c_votantes * 0.15, 0))
+
+		self.votos = {
+		1: [],
+		2: [],
+		3: [],
+		4: [],
+		5: []
+		} # registro de [n, n, n], [n, n, n]
+	def simular(self):
+		"""
+		lleva a cabo la simulación total
+		del número de votantes dados
+		"""
+		c_pseudo = 0
+		for sector in self.votos_sector.items():
+			print("sector {}: {} votantes".format(sector[0], sector[1]))
+			for i in range(0, sector[1]): #por cada votante en el sector
+				for j in range(0, 3): # 3 votos
+					voto = self.votante.votar(self.numeros[c_pseudo], self.distro)
+					c_pseudo += 1
+					if j == 2:
+						self.registrar_voto(sector[0], self.votante.votos)
+						print(self.votante.votos)
+
+		print("sector 1: ", self.analizar_sector(1))
+
+
+	def registrar_voto(self, n_sector,  voto):
+		"""Registra el voto segun el n_sector al que pertence
+		"""
+		if len(voto) == 3:
+			self.votos[n_sector].append(voto)
+
+	def analizar_sector(self, n_sector):
+		"""Regresa las 5 opciones mas votadas y su cantidad
+		de votos, el algoritmo comienza de forma vertical por
+		orden de importancia (la primera columna es mas importante),
+		y luego hace un conteo global
+		"""
+		prohibidos = [] #numeros eliminados
+		votos = self.votos[n_sector]
+		col_uno = []
+		for i in range(0, len(votos)): # columna uno
+			col_uno.append(votos[i][0])
+		col_dos = []
+		for i in range(0, len(votos)):
+			col_dos.append(votos[i][1]) #columna dos
+		col_tres = []
+		for i in range(0, len(votos)):
+			col_tres.append(votos[i][2]) # columna 3
+		#print(col_uno, col_dos, col_tres)
+
+		uno = statistics.mode(col_uno)
+		prohibidos.append(uno)
+		#print(col_uno)
+		#print("uno: ", uno)
+		for numero in prohibidos:
+			borrar_numero(col_dos, numero)
+		#print(col_dos)
+		dos = statistics.mode(col_dos)
+		prohibidos.append(dos)
+		#print("dos: ", dos)
+		#print(col_tres)
+		for numero in prohibidos:
+			borrar_numero(col_tres, numero)
+		#print(col_tres)
+		tres = statistics.mode(col_tres)
+		prohibidos.append(tres)
+		#print("tres: ", tres)
+
+		vector = [] #todos los votos
+		for voto in votos:
+			for opcion in voto:
+				vector.append(opcion)
+
+		#print("vector ", vector)
+		borrar_numero(vector, uno)
+		borrar_numero(vector, dos)
+		borrar_numero(vector, tres)
+		#print("vector ", vector)
+
+		cuatro = statistics.mode(vector)
+		#print("cuatro", cuatro)
+		borrar_numero(vector, cuatro)
+		cinco = statistics.mode(vector)
+		#print("cinco", cinco)
+		borrar_numero(vector,cinco)
+		#print(vector)
+
+		return [uno, dos, tres, cuatro, cinco]
+
+#nota: si quiero implementar mi moda, entonces 
+#lo mejor es acomodarlos de abajo hacia arriba
+
+	def contar_votos_sector(self, n_sector, opcion):
+		"""cuenta cuantos votos hubo para la opcion
+		dada en el sector dado"""
+
+	def vector_votos(self, n_sector):
+		votos = self.votos[n_sector]
+		vector = [] #todos los votos
+		for voto in votos:
+			for opcion in voto:
+				vector.append(opcion)
+		return vector
+
+
+def borrar_numero(arreglo, numero):
+	if numero in arreglo:
+		for i in arreglo:
+			if i == numero:
+				arreglo.remove(numero)
+	if numero in arreglo: #debe ser luego del ciclo
+		arreglo.remove(numero) #siempre uno residual
 
 def main():
 	print("elecciones!")
@@ -133,17 +283,14 @@ def main():
 	gen.ciclo(300)
 	
 	votante = Votante()
-	for i in range(0, 3):
+	for i in range(0, 100):
 		pseudo = gen.next()
 		print("pseudo", pseudo)
 		voto = votante.votar(pseudo, distro)
 		if votante.contador_votos == N_VOTOS:
 			print(votante.votos)
-		"""if voto:
-			print(votante.votos, votante.contador_votos)
-		else:
-			print("reinicio!")
-			"""
+	votacion = Votacion(100, distro, gen.get_generacion())
+	votacion.simular()
 		
 
 if __name__ == '__main__':
